66c0d24094c4946b4f7c2eee2bb5616fa10b83a1
QadirSalman519 <qadirsalman019@gmail.com>
2025-09-29 23:05:10 +0500

    Breadcrums Added

--- src/components/Layout/DashboardLayout.jsx
+++ src/components/Layout/DashboardLayout.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { Layout } from 'antd';
-import Navbar from './Navbar/Navbar';
-import AppHeader from './Header/Header';
-import AppFooter from './Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { useState } from "react";
+import { Layout } from "antd";
+import Navbar from "./Navbar/Navbar";
+import AppHeader from "./Header/Header";
+import AppFooter from "./Footer/Footer";
+import { Outlet } from "react-router-dom";
+import Breadcrumbs from "../Common/Breadcrums/Breadcrumbs";
 
 const { Content } = Layout;
 
@@ -11,14 +12,15 @@ function DashboardLayout() {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Navbar collapsed={collapsed} onCollapse={setCollapsed} />
 
       <Layout>
         <AppHeader collapsed={collapsed} setCollapsed={setCollapsed} />
 
-        <Content style={{ margin: '16px' }}>
-          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+        <Content style={{ margin: "16px" }}>
+          <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+            <Breadcrumbs />
             <Outlet />
           </div>
         </Content>
